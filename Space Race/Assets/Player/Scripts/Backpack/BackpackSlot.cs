54a959b0507045ba790cca71a5dffddcc081d423
Royd54 <duponroy@gmail.com>
2020-09-28 22:40:27 +0200

    Interaction changes
    Got rid of the way I handled interactions before this. Came up with a new way to handle the movements of the hands, because the previous one was stuttery, and caused a lot of problems. This was caused by the rigidbodies not being able to act smooth with the library. Also worked on the backpack for a bit, and added the function to put a hat on your head and put the backpack on your back. And last but not least I started working on the weapon holsters.

---
+++ Space Race/Assets/Player/Scripts/Backpack/BackpackSlot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class BackpackSlot : XRSocketInteractor
+{
+    private GameObject _inSocket = null;
+    private Vector3 _inSocketScale = Vector3.zero;
+    private XRSocketInteractor _socket = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _socket = GetComponent<XRSocketInteractor>();
+    }
+
+
+    protected override void OnSelectEnter(XRBaseInteractable interactable)
+    {
+        base.OnSelectEnter(interactable);
+        _inSocketScale = _inSocket.transform.localScale;
+        if (_inSocket.tag != "Player")
+            _inSocket.transform.localScale = gameObject.transform.localScale;
+            
+    }
+
+    protected override void OnSelectExit(XRBaseInteractable interactable)
+    {
+        base.OnSelectExit(interactable);
+        _inSocket.transform.localScale = _inSocketScale;
+        _inSocketScale = Vector3.zero;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        _inSocket = other.gameObject;
+    }
+
+
+}
---
+++ Space Race/Assets/Player/Scripts/Backpack/FollowPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private Vector3 _offset;
+
+    public int characterSlots = 0;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (characterSlots == 1)
+        {
+            transform.position = _target.position + Vector3.up * _offset.y
+             + Vector3.ProjectOnPlane(_target.right, Vector3.up).normalized * _offset.x
+             + Vector3.ProjectOnPlane(_target.forward, Vector3.up).normalized * _offset.z;
+
+            transform.eulerAngles = new Vector3(0, _target.eulerAngles.y, 0);
+        }else
+        {
+            transform.position = _target.transform.position + _offset;
+        }
+    }
+}

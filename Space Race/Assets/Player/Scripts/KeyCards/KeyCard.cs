acfbb2887ef887c3055442ceca6babbfe34448e5
Royd54 <duponroy@gmail.com>
2020-10-10 20:15:36 +0200

    Key Functionality
    Added a simple key system that opens doors based on the enum value's of the door/key.

---
+++ Space Race/Assets/Player/Scripts/KeyCards/KeyCard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCard : MonoBehaviour
+{
+    [SerializeField] private KeyCardType _keyCardType;
+
+    public enum KeyCardType
+    {
+        Red,
+        Blue,
+        Brown
+    }
+
+    public KeyCardType GetKeyCardType()
+    {
+        return _keyCardType;
+    }
+}
---
+++ Space Race/Assets/Player/Scripts/KeyCards/KeyCardDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCardDoor : MonoBehaviour
+{
+    [SerializeField] private KeyCard.KeyCardType _keyCardType;
+    [SerializeField] private GameObject _doorPart;
+    [SerializeField] private GameObject _endPos;
+    [SerializeField] private float _openSpeed;
+    [SerializeField] private bool _doorIsOpened = false;
+
+    public KeyCard.KeyCardType GetKeyCardType()
+    {
+        return _keyCardType;
+    }
+
+    private void Update()
+    {
+        if (_doorIsOpened) OpenDoor();
+    }
+
+    private void OpenDoor()
+    {
+        _doorPart.transform.position = Vector3.Lerp(_doorPart.transform.position, _endPos.transform.position, _openSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        KeyCard keyCard = other.GetComponent<KeyCard>();
+        if (keyCard != null)
+        {
+            if (keyCard.GetKeyCardType() == GetKeyCardType())
+            {
+                _doorIsOpened = true;
+            }
+        }
+    }
+}
